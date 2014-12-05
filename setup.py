539d2d246184b2fab8454682dc2abb2e8e8c5c81
Taurus Olson <taurusolson@gmail.com>
2014-12-05 22:45:29 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+from codecs import open as codecs_open
 from setuptools import setup
 
-setup(
-        name="fntools",
-        author="Damien Rabois",
-        version="1.0",
-        py_modules=["fntools"],
-        description="Functional programming tools for data processing"
-        )
+
+# Get the long description from the relevant file
+with codecs_open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='fntools',
+      description='Functional programming tools for data processing',
+      author=u'Taurus Olson',
+      author_email=u'taurusolson@gmail.com',
+      url='https://github.com/TaurusOlson/fntools',
+      version='1.0',
+      py_modules=['fntools'],
+      packages=['fntools'],
+      zip_safe=False
+      )
 
