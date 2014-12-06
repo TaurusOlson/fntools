6be9ff52456a27eadc6baa3ce6f6846085675ca4
Taurus Olson <taurusolson@gmail.com>
2014-12-06 03:57:26 +0100

    Add functions into __init__.py

---
+++ fntools/__init__.py
@@ -0,0 +1,7 @@
+from .fntools import use_with, zip_with, unzip, concat, mapcat, dmap, compose,\
+        groupby, reductions, split, assoc, dispatch, multimap, pipe,\
+        pipe_each, shift, duplicates, pluck, use, get_in, valuesof, valueof,\
+        take, drop, find, isiterable, are_in, any_in, all_in, monotony,\
+        attributes, find_each, dfilter, occurrences, indexof, indexesof,\
+        count, isdistinct
+
