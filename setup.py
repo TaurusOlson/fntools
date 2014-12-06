e0bbdbcd3c0f4fb1a6893471f5480b1b32a74fe1
Taurus Olson <taurusolson@gmail.com>
2014-12-06 21:19:00 +0100

    Add more metadata for the package on PyPi

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
 
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(name='fntools',
+      version='1.0.4',
       description='Functional programming tools for data processing',
+      long_description=readme(),
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+          ],
       author=u'Taurus Olson',
       author_email=u'taurusolson@gmail.com',
       url='https://github.com/TaurusOlson/fntools',
-      version='1.0.4',
       packages=['fntools'],
+      keywords='functional programming tools data processing',
+      license='MIT',
+      include_package_data=True,
       zip_safe=False
       )
