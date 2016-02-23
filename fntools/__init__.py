985c096dc811cdbf7b125b71e75ade72fc73ee40
Taurus Olson <taurusolson@gmail.com>
2016-02-23 17:32:05 +0400

    Update import in __init__.py

--- fntools/__init__.py
+++ fntools/__init__.py
@@ -4,12 +4,12 @@ fntools: functional programming tools for data processing
 
 """
 
-from .fntools import use_with, zip_with, unzip, concat, mapcat, dmap, compose,\
-        groupby, reductions, split, assoc, dispatch, multimap, pipe,\
-        pipe_each, shift, duplicates, pluck, use, get_in, valuesof, valueof,\
-        take, drop, find, isiterable, are_in, any_in, all_in, monotony,\
-        attributes, find_each, dfilter, occurrences, indexof, indexesof,\
-        count, isdistinct
+from .fntools import use_with, zip_with, unzip, concat, mapcat, dmap, rmap, replace,\
+        compose, groupby, reductions, split, assoc, dispatch, multimap,\
+        multistarmap, pipe, pipe_each, shift, repeatedly, duplicates, pluck, use, get_in,\
+        valuesof, valueof, take, drop, find, select, isiterable, are_in, any_in,\
+        all_in, monotony, attributes, find_each, dfilter, occurrences,\
+        indexof, indexesof, count, isdistinct
 
 
 __version__ = '1.0.4'
