e620c96f9a5bbf2d42479b7a485ec824dafe1e99
Taurus Olson <taurusolson@gmail.com>
2016-03-02 12:04:12 +0100

    Fix mapcat

--- fntools/fntools.py
+++ fntools/fntools.py
@@ -68,6 +68,11 @@ def mapcat(fn, colls):
 
     :param fn: a function
     :param colls: a list of collections
+    :returns: a list
+
+    >>> mapcat(reversed,  [[3, 2, 1, 0], [6, 5, 4], [9, 8, 7]])
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
     """
     return map(fn, concat(colls))
 
