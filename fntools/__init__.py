41ac4d46b97d3b8550e40cd3083d187d8a9a9c54
Taurus Olson <taurusolson@gmail.com>
2016-03-04 16:51:09 +0100

    Minor fix

--- fntools/__init__.py
+++ fntools/__init__.py
@@ -6,11 +6,11 @@ fntools: functional programming tools for data processing
 
 from .fntools import use_with, zip_with, unzip, concat, mapcat, dmap, rmap, replace,\
         compose, groupby, reductions, split, assoc, dispatch, multimap,\
-        multistarmap, pipe, pipe_each, shift, repeatedly, update, duplicates,
-        pluck, pluck_each, use, get_in,\
-        valueof, take, drop, find, remove, isiterable, are_in, any_in,\
-        all_in, monotony, attributes, find_each, dfilter, occurrences,\
-        indexof, indexesof, count, isdistinct, nrow, ncol, names
+        multistarmap, pipe, pipe_each, shift, repeatedly, update, duplicates,\
+        pluck, pluck_each, use, get_in, valueof, take, drop, find, remove,\
+        isiterable, are_in, any_in, all_in, monotony, attributes, find_each,\
+        dfilter, occurrences, indexof, indexesof, count, isdistinct, nrow,\
+        ncol, names
 
 
 __version__ = '1.1.1'
